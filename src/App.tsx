27ec8bf97fdefb8580f26ef4807053343ca57b3e
Sébastien <sgunther@laposte.net>
2022-11-06 11:23:00 +0100

    Add responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
         <Title order={1}> Complete informations </Title>
 
         <Grid columns={3} gutter="xl" mt="1px">
-          <Grid.Col span={2}>
+          <Grid.Col md={2} sm={3}>
             <Text>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
               Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
@@ -15,44 +15,44 @@ function App() {
           </Grid.Col>
         </Grid>
 
-        <Grid columns={3} gutter="xl" mt="xl">
-          <Grid.Col span={1}>
+        <Grid columns={6} gutter="xl" mt="xl">
+          <Grid.Col md={2} sm={3} xs={6}>
             Upload
           </Grid.Col>
         </Grid>
 
-        <Grid columns={3} gutter="xl" mt="sm">
-          <Grid.Col span={1}>
+        <Grid columns={6} gutter="xl" mt="sm">
+          <Grid.Col md={2} sm={3} xs={6}>
             Blockchain
           </Grid.Col>
-          <Grid.Col span={1}>
+          <Grid.Col md={2} sm={3} xs={6}>
             Protocol
           </Grid.Col>
         </Grid>
 
-        <Grid columns={3} gutter="xl" mt="sm">
-          <Grid.Col span={1}>
+        <Grid columns={6} gutter="xl" mt="sm">
+          <Grid.Col md={2} sm={3} xs={6}>
             Name
           </Grid.Col>
-          <Grid.Col span={1}>
+          <Grid.Col md={2} sm={3} xs={6}>
             Symbol
           </Grid.Col>
-          <Grid.Col span={1}>
+          <Grid.Col md={2} sm={3} xs={6}>
             Amount
           </Grid.Col>
         </Grid>
 
-        <Grid columns={3} gutter="xl" mt="sm">
-          <Grid.Col span={1}>
+        <Grid columns={6} gutter="xl" mt="sm">
+          <Grid.Col md={2} sm={3} xs={6}>
             Owner
           </Grid.Col>
-          <Grid.Col span={1}>
+          <Grid.Col md={2} sm={3} xs={6}>
             <Anchor href="#"> More options </Anchor>
           </Grid.Col>
         </Grid>
 
         <Grid columns={3} gutter="xl" mt="sm">
-          <Grid.Col span={2}>
+          <Grid.Col md={2} sm={3}>
             Description
           </Grid.Col>
         </Grid>
