f76872e36f9661e0f623bd9dfe758e17eadd083a
Sébastien <sgunther@laposte.net>
2022-11-06 18:27:16 +0100

    Add global styles in mantine provider
    width-100

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,17 @@ import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <MantineProvider withGlobalStyles withNormalizeCSS>
+    <MantineProvider 
+      theme={{ 
+        globalStyles: () => ({
+          '#app .width-100': {
+            width: '100%'
+          }
+        })
+      }}
+      withGlobalStyles
+      withNormalizeCSS
+    >
       <App />
     </MantineProvider>
   </React.StrictMode>
