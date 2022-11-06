73da41b1b0ce4325a557377bd0ffc8333b9fe31b
Sébastien <sgunther@laposte.net>
2022-11-06 01:04:44 +0100

    Add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json']
+    },
+    plugins: ['react', '@typescript-eslint'],
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking'
+    ],
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    },
+    rules: {}
+};
