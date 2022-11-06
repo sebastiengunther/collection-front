f4d02b35797c5f94a33d651ac698c92e882d486c
Sébastien <sgunther@laposte.net>
2022-11-06 21:48:56 +0100

    Add constants to do the validation of the form

--- src/Const.ts
+++ src/Const.ts
@@ -1,11 +1,25 @@
 export default class {
-  public static readonly ADDRESS_BEGIN_DOT = 22;
-  public static readonly ADDRESS_END_DOT = 34;
-  public static readonly ADDRESS_LEN = 42;
+  public static readonly MAX_FILES = 1;
+  public static readonly FILE_MAX_SIZE = 100_000_000;
+  public static readonly B_TO_MB = 1_000_000;
+  
+  public static readonly NAME_MIN_LEN = 3;
+  public static readonly NAME_MAX_LEN = 25;
+  
+  public static readonly SYMBOL_MIN_LEN = 1;
+  public static readonly SYMBOL_MAX_LEN = 3;
+
   public static readonly AMOUNT_MIN = 1;
   public static readonly AMOUNT_MAX = 10_000;
+
+  public static readonly ADDRESS_BEGIN_DOT = 22;
+  public static readonly ADDRESS_END_DOT = 34;
+  public static readonly ADDRESS_LENGTH = 42;
+
+  public static readonly DESCRIPTION_MIN_LEN = 20;
+  public static readonly DESCRIPTION_MAX_LEN = 2000;
+
   public static readonly ETH_REQUEST_ACCOUNT = 'eth_requestAccounts';
-  public static readonly FILE_MAX_SIZE = 100_000_000;
+
   public static readonly ICON_INFO_SIZE = 18;
-  public static readonly MAX_FILES = 1;
 }
