8ea04d102907f759ac076e1d8ce471c2b1d33a26
Sébastien <sgunther@laposte.net>
2022-11-06 21:07:53 +0100

    Add constant for eth request account

--- src/Const.ts
+++ src/Const.ts
@@ -1,6 +1,7 @@
 export default class {
   public static readonly AMOUNT_MIN = 1;
   public static readonly AMOUNT_MAX = 10_000;
+  public static readonly ETH_REQUEST_ACCOUNT = 'eth_requestAccounts';
   public static readonly FILE_MAX_SIZE = 100_000_000;
   public static readonly ICON_INFO_SIZE = 18;
   public static readonly MAX_FILES = 1;
