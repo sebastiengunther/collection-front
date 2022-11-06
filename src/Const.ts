937d3dc80f3bc62a16e8a85197682c8b1f4ac9bf
Sébastien <sgunther@laposte.net>
2022-11-06 19:43:48 +0100

    Add constant for max files

--- src/Const.ts
+++ src/Const.ts
@@ -3,4 +3,5 @@ export default class {
   public static readonly AMOUNT_MAX = 10_000;
   public static readonly FILE_MAX_SIZE = 100_000_000;
   public static readonly ICON_INFO_SIZE = 18;
+  public static readonly MAX_FILES = 1;
 }
